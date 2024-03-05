f72c71c591ba5d48291781bb5ea92415591ef15f
kite <2257440643@qq.com>
2024-03-05 22:33:15 +0800

    feat(reactivity): track and trigger

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,15 +1,38 @@
-const get = (target: object, key: string | symbol, receiver: object) => {
-  console.log('get', target, key)
+import { track, trigger } from './effect'
+
+const get = createGretter()
+/**
+ * 创建 getter 回调方法
+ */
+function createGretter() {
+  return function get(target: object, key: string | symbol, receiver: object) {
+    // 利用Reflect得到返回值
+    const res = Reflect.get(target, key, receiver)
+    // 收集依赖
+    track(target, key)
+
+    return res
+  }
 }
 
-const set = (
-  target: object,
-  key: string | symbol,
-  value: unknown,
-  receiver: object
-) => {
-  console.log('set', target, key, value)
-  return true
+const set = createSetter()
+/**
+ * 创建 setter 回调方法
+ */
+function createSetter() {
+  return function set(
+    target: object,
+    key: string | symbol,
+    value: unknown,
+    receiver: object
+  ) {
+    // 利用 Reflect 设置新值
+    const res = Reflect.set(target, key, value, receiver)
+    // 触发依赖
+    trigger(target, key, value)
+
+    return res
+  }
 }
 
 /**
---
+++ packages/reactivity/src/effect.ts
@@ -0,0 +1,66 @@
+/**
+ * 收集所有依赖的 WeakMap 实例：
+ * 1. `key`：响应性对象
+ * 2. `value`：`Map` 对象
+ * 		1. `key`：响应性对象的指定属性
+ * 		2. `value`：指定对象的指定属性的 执行函数
+ */
+const targetMap = new WeakMap<any, Map<any, ReactiveEffect>>()
+
+export function track(target: object, key: string | symbol) {
+  // 如果当前不存在执行函数，直接return
+  if (!activeEffect) return
+  // 尝试根据 target 从 targetMap 中获取map
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    // 不存在，则生成新的 map 对象，并把该对象赋值给对应的 value
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  let dep = depsMap.get(key)
+  if (!dep) {
+    depsMap.set(key, (dep = activeEffect))
+  }
+}
+export function trigger(target: object, key: string | symbol, value: unknown) {
+  // 依据 target 从 targetMap 中获取存储的 map 实例
+  const depsMap = targetMap.get(target)
+  if (!depsMap) {
+    return
+  }
+  // 依据指定的 key，获取 dep 实例
+  let dep: ReactiveEffect | undefined = depsMap.get(key)
+  // dep 不存在则直接 return
+  if (!dep) {
+    return
+  }
+  // 触发 effect
+  dep.run()
+}
+
+/**
+ * 单例的，当前激活的 effect
+ */
+export let activeEffect: ReactiveEffect | undefined
+/**
+ * 响应性触发依赖时的执行类
+ */
+export class ReactiveEffect<T = any> {
+  constructor(public fn: () => T) {}
+
+  run() {
+    // 为 activeEffect 赋值
+    activeEffect = this
+
+    return this.fn()
+  }
+}
+/**
+ * effect 函数
+ * @param fn 执行方法
+ * @returns 以 ReactiveEffect 实例为 this 的执行函数
+ */
+export function effect<T = any>(fn: () => T) {
+  // 生成 ReactiveEffect 实例
+  const _effect = new ReactiveEffect(fn)
+  _effect.run()
+}
