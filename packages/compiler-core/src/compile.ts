55b2a894a1d3692e144de193f75f808f2cd01e74
kite <2257440643@qq.com>
2024-03-15 21:27:05 +0800

    feat: genrete render function after transform

--- packages/compiler-core/src/compile.ts
+++ packages/compiler-core/src/compile.ts
@@ -3,6 +3,7 @@ import { baseParse } from "./parse";
 import { transform } from "./transform";
 import { transformElement } from "./transforms/transformElement";
 import { transformText } from "./transforms/transformText";
+import { generate } from "./codegen";
 
 export function baseCompile(template: string, options = {}) {
   const ast = baseParse(template.trim())
@@ -14,5 +15,5 @@ export function baseCompile(template: string, options = {}) {
     })
   )
 
-  return ast
+  return generate(ast)
 }
--- packages/compiler-core/src/utils.ts
+++ packages/compiler-core/src/utils.ts
@@ -1,4 +1,5 @@
 import { NodeTypes } from './ast'
+import { CREATE_ELEMENT_VNODE, CREATE_VNODE } from './runtimeHelpers'
 
 export function isText(node) {
 	return node.type === NodeTypes.INTERPOLATION || node.type === NodeTypes.TEXT
@@ -14,4 +15,11 @@ export function createObjectExpression(properties) {
 		loc: {},
 		properties
 	}
+}
+
+/**
+ * 返回 vnode 生成函数
+ */
+export function getVNodeHelper(ssr: boolean, isComponent: boolean) {
+	return ssr || isComponent ? CREATE_VNODE : CREATE_ELEMENT_VNODE
 }
