6c172884524012517134ede51d338b4f8002a6ce
kite <2257440643@qq.com>
2024-03-16 16:32:06 +0800

    feat: 组件中不包含 render 函数，同时包含 template 模板，则需使用编辑器进行编辑得到 render 函数

--- packages/runtime-core/src/component.ts
+++ packages/runtime-core/src/component.ts
@@ -5,6 +5,19 @@ import { onBeforeMount, onMounted } from './apiLifecycle'
 let uid = 0
 
 /**
+ * 编辑器实例
+ */
+let compile
+
+/**
+ * 用来注册编译器的运行时
+ */
+export function registerRuntimeCompiler(_compile: any) {
+	compile = _compile
+}
+
+
+/**
  * 生命周期钩子
  */
 export const enum LifecycleHooks {
@@ -69,11 +82,20 @@ export function handleSetupResult(instance, setupResult) {
 	finishComponentSetup(instance)
 }
 
+
 export function finishComponentSetup(instance) {
 	const Component = instance.type
 
 	// 组件不存在 render 时，才需要重新赋值
 	if (!instance.render) {
+		// 存在编辑器，并且组件中不包含 render 函数，同时包含 template 模板，则直接使用编辑器进行编辑，得到 render 函数
+		if (compile && !Component.render) {
+			if (Component.template) {
+				// 这里就是 runtime 模块和 compile 模块结合点
+				const template = Component.template
+				Component.render = compile(template)
+			}
+		}
 		// 为 render 赋值
 		instance.render = Component.render
 	}
